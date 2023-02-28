abdaeaa537b762036e8b360f6927df70039e837c
Aamir Shaikh <aamir0744@gmail.com>
2023-02-28 16:03:42 +0000

    working on index page, got images sorted and filter boxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get '/index', to: "cars#index"
+  get '/cars', to: "cars#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'wiping db clean'
-User.destroy_all
 Car.destroy_all
+User.destroy_all
 
 puts 'creating fake records'
 user = User.new(email: 'testing123@gmail.com',encrypted_password: 'something_long1234**!', first_name: 'aamir', last_name: 'shaikh', role: 'owner')
 user.save
-car1 = Car.new(model: 'Tesla', availability: true)
+car1 = Car.new(model: 'https://m.media-amazon.com/images/I/61foocSpTxL._AC_SL1500_.jpg', availability: true)
 car1.user = user
 car1.save
-car2 = Car.new(model: 'Ferrari', availability: true)
+car2 = Car.new(model: 'https://m.media-amazon.com/images/I/61foocSpTxL._AC_SL1500_.jpg', availability: true)
 car2.user = user
 car2.save
-car3 = Car.new(model: 'Lamborghini', availability: true)
+car3 = Car.new(model: 'https://m.media-amazon.com/images/I/61foocSpTxL._AC_SL1500_.jpg', availability: true)
 car3.user = user
 car3.save
 puts 'finished'
