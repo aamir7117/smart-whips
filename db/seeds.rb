42b0f4334d19703afb3301e872f598a8311b1ae4
Aamir Shaikh <aamir0744@gmail.com>
2023-02-28 12:34:51 +0000

    got seed working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'wiping db clean'
+User.destroy_all
+Car.destroy_all
+
+puts 'creating fake records'
+user = User.new(email: 'testing123@gmail.com',encrypted_password: 'something_long1234**!', first_name: 'aamir', last_name: 'shaikh', role: 'owner')
+user.save
+car1 = Car.new(model: 'Tesla', availability: true)
+car1.user = user
+car1.save
+car2 = Car.new(model: 'Ferrari', availability: true)
+car2.user = user
+car2.save
+car3 = Car.new(model: 'Lamborghini', availability: true)
+car3.user = user
+car3.save
+puts 'finished'
