b5eb444e86547525a8b7b8eaf70065687f47627f
Ivan Brown <brotomann@gmail.com>
2023-03-02 12:15:55 +0000

    improved seed with random images for each car.  removed deprecated instances of car.image_url as we now use car.photo.key

--- db/migrate/20230228152635_add_image_url_to_cars.rb
+++
@@ -1,5 +0,0 @@
-class AddImageUrlToCars < ActiveRecord::Migration[7.0]
-  def change
-    add_column :cars, :image_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_113532) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
